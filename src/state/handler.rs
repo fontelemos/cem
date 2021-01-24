5f42f59a7f4f400e14c9b6779da0898edb18c1c8
Leinvedan <fontebasso.lemos@gmail.com>
2021-01-24 19:29:56 -0300

    feat: add block struct with tests

--- src/state/handler.rs
+++ src/state/handler.rs
@@ -1,7 +1,9 @@
 use json_patch::merge as json_merge;
+use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
+use std::collections::HashMap;
 
-pub fn convert(content: &str) -> Value {
+fn convert(content: &str) -> Value {
     json!(content)
 }
 
@@ -9,4 +11,33 @@ pub fn merge(content: Value, patch: Value) -> Value {
     let mut original_copy = content.clone();
     json_merge(&mut original_copy, &patch);
     original_copy
+}
+
+pub fn build_state() -> HashMap<String, Value> {
+    HashMap::new()
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Block {
+    pub id: String,
+    pub content: Value
+}
+
+impl From<&str> for Block {
+    fn from(block_string: &str) -> Block {
+        let empty_block = Block { 
+            id: "" .to_string(),
+            content: convert("{}")
+        };
+        let block: Block = serde_json::from_str(block_string)
+            .unwrap_or(empty_block);
+        block
+    }
+}
+
+impl Block {
+    pub fn is_valid(&self) -> bool {
+        let invalid_id = String::from("");
+        self.id != invalid_id
+    }
 }
--- tests/state_test.rs
+++ tests/state_test.rs
@@ -1,35 +1,94 @@
-use cem::state::handler::{convert, merge};
 #[cfg(test)]
+use cem::state::handler::{build_state, merge, Block};
 use serde_json::json;
 
 #[test]
-fn state_from_method_test() {
+fn content_merge_test() {
   let original_state_str = r#"{
-      "name": {
-        "hash": "2230o363glhhh64",
-        "generation": 243,
-        "display_name": "Uruk the big barbarian" 
-      },
-    }"#;
+    "id": "soiadj9087asdbnjk",
+    "content": {
+      "hash": "2230o363glhhh64",
+      "generation": 243,
+      "display_name": "Uruk the big barbarian",
+      "attack": {
+        "axe": "100dmg"
+      }
+    }
+  }"#;
   let new_state = json!(
     r#"{
-      "name": {
+      "id": "soiadj9087asdbnjk",
+      "content": {
         "hash": "2230o363glhhh64",
         "generation": 244,
-        "display_name": "Uruk the very big barbarian!" 
-      },
+        "display_name": "Uruk the very big barbarian!",
+        "attack": {}
+      }
     }"#
   );
   let expected_state = json!(
     r#"{
-      "name": {
+      "id": "soiadj9087asdbnjk",
+      "content": {
         "hash": "2230o363glhhh64",
         "generation": 244,
-        "display_name": "Uruk the very big barbarian!" 
-      },
+        "display_name": "Uruk the very big barbarian!",
+        "attack": {}
+      }
     }"#
   );
-  let state = convert(original_state_str);
-  let result = merge(state, new_state);
+  let block = Block::from(original_state_str);
+  let result = merge(block.content.clone(), new_state);
   assert_eq!(expected_state, result)
+}
+
+#[test]
+fn receive_and_store_state() {
+  let original_state_str = r#"{
+      "id": "soiadj9087asdbnjk",
+      "content": {
+        "hash": "2230o363glhhh64",
+        "generation": 243,
+        "display_name": "Uruk the big barbarian" 
+      }
+    }"#;
+  let mut state = build_state();
+  let block = Block::from(original_state_str);
+  assert!(block.is_valid());
+  assert_eq!(state.insert(block.id.clone(), block.content.clone()), None);
+  let expected_id: String = String::from("soiadj9087asdbnjk");
+  assert_eq!(block.id, expected_id);
+}
+
+#[test]
+fn invalid_state() {
+  let state1_str = r#"{
+      "id": "soiadj9087asdbnjk",
+      "contento": {
+        "hash": "2230o363glhhh64",
+        "generation": 243,
+        "display_name": "Uruk the big barbarian" 
+      }
+    }"#;
+  let state2_str = r#"{
+      "content": {
+        "hash": "2230o363glhhh64",
+        "generation": 243,
+        "display_name": "Uruk the big barbarian" 
+      }
+    }"#;
+  let state3_str = r#"{
+      "id": "",
+      "content": {
+        "hash": "2230o363glhhh64",
+        "generation": 243,
+        "display_name": "Uruk the big barbarian" 
+      }
+    }"#;
+  let block1 = Block::from(state1_str);
+  let block2 = Block::from(state2_str);
+  let block3 = Block::from(state3_str);
+  assert_eq!(block1.is_valid(), false);
+  assert_eq!(block2.is_valid(), false);
+  assert_eq!(block3.is_valid(), false);
 }
